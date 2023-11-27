08f096bde64e65e30615b9ec3c6b55910d5b0c06
necOjy <jim2323098@gmail.com>
2023-11-27 18:12:31 +0800

    初始程式碼

---
+++ insert.php
@@ -0,0 +1,15 @@
+<?php
+header("Content-type: text/html;charset=utf-8");
+include_once "db_conn.php";
+$name = $_POST["name"];
+$website = $_POST["website"];
+$phone = $_POST["phone"];
+$address = $_POST["address"];
+$hour = $_POST["business_hour"];
+
+
+$query = ("insert into restaurant values(?,?,?,?,?)");
+$stmt = $db->prepare($query);
+$stmt->execute(array($name,$website,$phone,$address,$hour)); 
+header("Location: inform.php");
+?>  

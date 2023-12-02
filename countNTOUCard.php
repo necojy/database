499ac22faa8933c85dd01c514689e5de1ece9b82
necOjy <jim2323098@gmail.com>
2023-12-02 15:21:37 +0800

    新增左側列表功能

--- countNTOUCard.php
+++ countNTOUCard.php
@@ -1,10 +1,11 @@
 <?php
 //統計能使用海大卡的店家個數
 include_once "db_conn.php";
-;
-$query = ("SELECT count (*) FROM category WHERE ntou_card = '是'");
+
+$query = ("SELECT count(distinct name) FROM category WHERE ntou_card = '是'");
 $stmt = $db->prepare($query);
 $stmt->execute();
-$result = $stmt->fetchAll();
-echo "能使用海大卡的店家個數: " . $count;
+$count = $stmt->fetchColumn();
+echo $count;
+
 ?> 
---
+++ showNCRestaurant.php
@@ -0,0 +1,19 @@
+<?php
+include_once "db_conn.php";
+$query = ("SELECT * FROM restaurant where name in (SELECT name FROM category WHERE ntou_card = '是')");
+$stmt = $db->prepare($query);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+echo count($result)."+";
+
+for ($i = 0; $i < count($result); $i++) 
+{
+    echo $result[$i]['name']."|"; // 若有 null ，將 null 換為空字串 ''
+    echo isset($result[$i]['website']) ? $result[$i]['website']."|" : ' '."|";
+    echo isset($result[$i]['phone']) ? $result[$i]['phone']."|" : ' '."|";
+    echo isset($result[$i]['address']) ? $result[$i]['address']."|" : ' '."|";
+    echo isset($result[$i]['business_hour']) ? $result[$i]['business_hour']: ' ';
+    echo "\n";
+}
+?>
